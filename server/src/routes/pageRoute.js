c4e5504ab6a065a015a5c7f9248b8c8b4a2c6f81
opusxdev <opusxdev@gmail.com>
2026-02-08 14:46:46 +0530

    fix router middleware

--- server/src/routes/pageRoute.js
+++ server/src/routes/pageRoute.js
@@ -1,15 +1,20 @@
-import  {Router}  from "express";
-import { registeration,login} from "../controllers/authController";
-import { newContent,content,deleteContent, shareContent } from "../controllers/crudController";
-import { isAuthenticated } from "../middleware/authMiddleware";
+import express from "express";
+import { registeration, login } from "../controllers/authController.js";
+import {
+  newContent,
+  content,
+  deleteContent,
+  shareContent
+} from "../controllers/crudController.js";
+import { isAuthenticated } from "../middleware/authMiddleware.js";
 
-const router = Router();
+const router = express.Router();
 
-router.get("/content",isAuthenticated,content)
-router.post("/signup",registeration)
-router.post("/signin",login)
-router.post("/addcontent",isAuthenticated,newContent)
-router.delete("/delete/:contentId",isAuthenticated,deleteContent)
-router.get("/share/:userId",isAuthenticated,shareContent)
+router.get("/content", isAuthenticated, content);
+router.post("/signup", registeration);
+router.post("/signin", login);
+router.post("/addcontent", isAuthenticated, newContent);
+router.delete("/delete/:contentId", isAuthenticated, deleteContent);
+router.get("/share/:userId", isAuthenticated, shareContent);
 
 export default router;
