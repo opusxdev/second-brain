2cb7f50a45c1e7781758391868e4120de5359eaf
opusxdev <opusxdev@gmail.com>
2026-02-08 17:25:01 +0530

    db connection and config

---
+++ server/src/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const dbConnect = () => {
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+      console.log("Connected Successfully")
+    })
+    .catch((err) => {
+      console.log("connection error", err);
+    })
+};
+
+export default dbConnect
